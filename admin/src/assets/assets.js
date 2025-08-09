a51e09e0d401f9c3cafa1e98987332502cdba645
gitAbhhi <abhiaditya926@gmail.com>
2025-08-09 23:26:11 +0530

    change backend url

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -16,4 +16,5 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'http://localhost:4000'
+// export const url = 'http://localhost:4000'
+export const url = 'https://food-ordering-website-mern.onrender.com'
